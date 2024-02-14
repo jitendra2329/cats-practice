b614b7c5ea79a7392656c7d6042d107a5d6940d3
jitendra2329 <jitendra.kumar@nashtechglobal.com>
2024-02-14 11:37:52 +0530

    initial commit

---
+++ src/main/scala/typeClasses/TypeClasses.scala
@@ -0,0 +1,34 @@
+package typeClasses
+
+object TypeClasses extends App {
+
+  trait Math[A]{
+    def add(first: A, second: A): A
+    def subtract(first: A, second: A): A
+    def multiply(first: A, second: A): A
+    def divide(first: A, second: A): A
+  }
+
+  implicit val addInts: Math[Int] = new Math[Int] {
+    override def add(first: Int, second: Int): Int = first + second
+
+    override def subtract(first: Int, second: Int): Int = first - second
+
+    override def multiply(first: Int, second: Int): Int = first * second
+
+    override def divide(first: Int, second: Int): Int = first / second
+  }
+
+  object DoTheMath {
+
+    private def addition[B](first: B, second: B)(implicit m: Math[B]): B = {
+      m.add(first, second)
+    }
+
+    println("Addition: "+ DoTheMath.addition(23,23))
+  }
+
+
+
+
+}
